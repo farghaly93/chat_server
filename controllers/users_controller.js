08f66bc93578e975a48eb36b55ea5e155304f02d
farghaly <miserable.farghaly93@gmail.com>
2021-01-08 17:23:08 +0200

    last

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,20 +6,32 @@ const bcrypt = require('bcrypt');
 const Cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 const { Buffer } = require('buffer');
+const { update, bulkWrite } = require('../models/messages.js');
+const { randomBytes } = require('crypto');
 Cloudinary.config({
     cloud_name: 'farghaly-developments',
     api_key: '789929815277853',
     api_secret: 'GRYCOy1KymmaOkGu6BuPVNH0VLc'
 });
 
+var nodemailer = require('nodemailer');
+const { reset } = require('nodemon');
+
+var transporter = nodemailer.createTransport({
+service: 'gmail',
+auth: {
+    user: 'miserable.farghaly93@gmail.com',
+    pass: 'saadawy_1993'
+}
+});
+
 exports.getAllMessages = async(req, res) => {
     try{
         const room = req.body.room;
-        const messagesPerView = req.body.messagesPerView;
-        const messagesToShow = req.body.messagesToShow;
+        const skip = req.body.skip;
+        const limit = req.body.limit;
         // return;
-        const messages = await Chat.find({room}).skip(messagesToShow).limit(messagesPerView).sort({date: -1});
-         console.log(room, messagesPerView, messagesToShow);
+        const messages = await Chat.find({room}).skip(skip).limit(limit).sort({date: -1});
         res.json({messages: messages});
     } catch(e) {
         console.log(e);
@@ -55,8 +67,9 @@ exports.register = async(req, res) => {
 exports.updateUser = async(req, res) => {
     try{
         const body = {...req.body};
-        const authData = await Users.updateOne({_id: body._id}, body);
-        if(authData.nModified === 1) {
+        const update = await Users.updateOne({_id: body._id}, body);
+        if(update.nModified === 1) {
+            const authData = await Users.findOne({_id: body._id});
             const token = jwt.sign({}, 'mohammadfarghalyalisaadawy', {expiresIn: '1000h'});
             res.json({userData: {
                 _id: authData._id,
@@ -70,6 +83,32 @@ exports.updateUser = async(req, res) => {
         }
     } catch(e) {
         console.log(e);
+    }
+}
+
+exports.changePassword = async(req, res) => {
+    try{
+        const body = req.body;
+        const account = await Users.findOne({_id: body.userId});
+        if(account != null) {
+            const isValid = bcrypt.compare(body.old, account.password);
+            if(!isValid) {
+                res.json({done: false});
+            } else {
+                const newPass = bcrypt.hashSync(body.new, 10);
+                const update = await Users.updateOne({_id: body.userId}, {password: newPass});
+                if(update.nModified === 1) {
+                    res.json({done: true});
+                } else {
+                    res.json({done: false});
+                }
+            }
+        } else {
+            res.json({done: false});
+        }
+    } catch(err) {
+        console.log(err);
+        res.json({done: false});
     }
 }
 
@@ -107,7 +146,6 @@ exports.login = async(req, res) => {
 exports.uploadFile = async(req, res) => {
     try{
         const image = req.files.file;
-        console.log(image.tempFilePath);
         Cloudinary.uploader.upload(image.tempFilePath, (err, result) => {
             if(err) {console.log(err); return;}
             res.json({url: result.secure_url});
@@ -120,7 +158,8 @@ exports.uploadAudio = async(req, res) => {
     try{
         const audio = req.files.file;
         console.log(audio.tempFilePath);
-        Cloudinary.uploader.upload_large(image.tempFilePath, (err, result) => {
+        // return;
+        Cloudinary.uploader.upload_large(audio.tempFilePath, (err, result) => {
             if(err) {console.log(err); return;}
             res.json({url: result.secure_url});
         });
@@ -169,23 +208,18 @@ exports.createRoom = async(req, res) => {
 
 exports.joinRoom = async(req, res) => {
     try {
-        console.log('jooiiiiiiiiiiiinnnnnnnnnnnn')
         const body = req.body;
         const isRoomExisted = await Rooms.findOne({room: body.room});
         if(isRoomExisted != null) {
-            console.log('Room found...');
             const isUserExistedInTheRoom = isRoomExisted.users.includes(body.userId);
             if(!isUserExistedInTheRoom) {
-                console.log('User not found in this room');
                 const add = await Rooms.updateOne({room: body.room}, {$push: {users: body.userId}});
                 if(add.nModified == 1) res.json({added: true});
                 else res.jdon({added: false});
             } else {
-                console.log('User found in this room');
                 res.json({added: true});
             }
         } else {
-            console.log('Room not found...');
             res.json({added: false});
         }
        
@@ -208,7 +242,6 @@ exports.deleteRoom = async(req, res) => {
     try{
         const delRoom = await Rooms.deleteOne({room: req.body.room, owner: req.body.userId});
         const delMessagesInRoom = await Chat.deleteMany({room: req.body.room});
-        console.log(delRoom.n, delMessagesInRoom.n);
         if(delRoom.n && (delMessagesInRoom.n || await Chat.find({room: req.body.room}).count() === 0)) {
             res.json({deleted: true});
         } else {
@@ -223,10 +256,47 @@ exports.deleteRoom = async(req, res) => {
 
 exports.getRoomData = async(req, res) => {
     try{
-        console.log('kkkkkkkkkkkkkkkkkkkk', req.body.room);
         const room = await Rooms.findOne({room: req.body.room});
-        console.log('rooooooooooom', room);
         res.json({roomData: room});
+    } catch(e) {
+        console.log(e);
+    }
+}
+exports.resetPassword = async(req, res) => {
+    try{
+        console.log(req.params.email);
+        randomBytes(16, async(err, hexaBytes) => {
+            // const decimalBytes = new Uint8Array(hexaBytes);
+            // let stringBinary = ''
+            // for(byte of decimalBytes) {
+            //     stringBinary += String.fromCharCode(byte);
+            // }
+            const newPassword = hexaBytes.toString('base64');
+            
+            const update = await Users.updateOne({email: req.params.email}, {password: bcrypt.hashSync(newPassword, 10)});
+            if(update.nModified === 1) {
+
+            var mailOptions = {
+            from: 'miserable.farghaly93@gmail.com',
+            to: req.params.email,
+            subject: 'your new password of farghaly_secret_chat',
+            text: newPassword
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+                res.json({done: false});
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.
+                json({done: true});
+                }
+            });
+        }
+        else {
+            res.json({done: false});
+        }
+    });
     } catch(e) {
         console.log(e);
     }
--- routes/users_router.js
+++ routes/users_router.js
@@ -9,12 +9,15 @@ Router.get('/getRoomsForUser/:userId', usersController.getRoomsForUser);
 Router.post('/getRoomData', usersController.getRoomData);
 Router.post('/register', usersController.register);
 Router.post('/updateUser', usersController.updateUser);
+Router.post('/changePassword', usersController.changePassword);
 Router.post('/login', usersController.login);
 Router.post('/deleteMessage', usersController.deleteMessage);
 Router.post('/createRoom', usersController.createRoom);
 Router.post('/joinRoom', usersController.joinRoom);
 Router.post('/deleteRoom', usersController.deleteRoom);
+Router.get('/resetPassword/:email', usersController.resetPassword);
 Router.post('/uploadFile', upload.single('file'), usersController.uploadFile);
+Router.post('/uploadAudio', upload.single('file'), usersController.uploadAudio);
 
 
 module.exports = Router;
